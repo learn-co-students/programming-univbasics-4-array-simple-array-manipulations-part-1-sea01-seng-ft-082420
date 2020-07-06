85503b19b31985706a2202292f5838e22bce03fe
Luke Hatcher <lukehatcher98@gmail.com>
2020-07-06 05:45:44 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(arr, str)
+  arr.push(str)  
+end
+
+def using_unshift(a, s)
+  a.unshift(s)
+end
+
+def using_pop(a)
+  a.pop()
+end
+
+def pop_with_args(a)
+  a.pop(2)
+end
+
+def using_shift(a)
+  return a.shift()
+end
+
+def shift_with_args(a)
+  a.shift(2)
+end
